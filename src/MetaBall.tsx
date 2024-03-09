2a9bc3c45f50f855f5c288ed6dc702b1e598243e
adamatronix <adam@adamatronix.com>
2024-03-09 17:23:30 +0800

    master More tweaks

--- src/MetaBall.tsx
+++ src/MetaBall.tsx
@@ -34,7 +34,7 @@ export const MetaBall = ({
       vec
         .copy(api.current.translation())
         .normalize()
-        .multiplyScalar(delta * -0.05),
+        .multiplyScalar(delta * -0.08),
     )
   })
 
